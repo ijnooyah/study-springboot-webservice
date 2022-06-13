6cba0121d0463666bd4be25a9fa2b7e4c4657206
yoonji <ijnooyah@gmail.com>
2022-06-13 12:38:16 +0900

    수정

--- src/main/java/com/ijnooyah/book/springboot/Application.java
+++ src/main/java/com/ijnooyah/book/springboot/Application.java
@@ -2,7 +2,10 @@ package com.ijnooyah.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+@EnableJpaAuditing // JPA AUditing 활성화
 @SpringBootApplication
 public class Application { // 앞으로 만들 프로젝트의 메인 클래스
     public static void main(String[] args) {
